ed781bdd25ccc9189a278c901291eef8d0772be1
JIMMY Clariel <31384852+Jimmy586@users.noreply.github.com>
2019-03-08 02:54:28 +0200

    getKey

--- blockchain/src/blockchain/Wallet.java
+++ blockchain/src/blockchain/Wallet.java
@@ -1,6 +1,7 @@
 package blockchain;
 
 import java.security.*;
+import java.security.spec.ECGenParameterSpec;
 
 public class Wallet {
 	public PublicKey publickey;
@@ -12,6 +13,22 @@ public class Wallet {
 		
 	}
 	
+	public void generateKeyPair() {
+		try {
+			KeyPairGenerator keyGen = KeyPairGenerator.getInstance("ECDSA","BC");
+			SecureRandom random = SecureRandom.getInstance("SHA1PRNG");
+			ECGenParameterSpec ecSpec = new ECGenParameterSpec("prime192v1");
+			// Initialize the key generator and generate a KeyPair
+			keyGen.initialize(ecSpec, random); //256
+	        KeyPair keyPair = keyGen.generateKeyPair();
+	        //Set the public and private keys from the keyPair
+	        this.privatekey = keyPair.getPrivate();
+	        this.publickey = keyPair.getPublic();
+	        
+		}catch(Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
 	void Update(float amount) {
 		this.Balance-=amount;
 	}
@@ -24,6 +41,10 @@ public class Wallet {
 	
 	public void writewallet(Wallet w) {
 		//process of writing w to Json file
+	}
+	
+	public PrivateKey getPrivateKey() {
+		return this.privatekey;
 	}
 	public float getBalance() {
 		return this.Balance;
